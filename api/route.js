6367652e098dcc891d54727b9a1c4c7bff6ee65c
Berke Güleryüz <berkeguleryuz@Berke-MacBook-Pro.local>
2024-04-04 09:52:24 +0300

    update

--- api/route.js
+++ api/route.js
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
         .status(500)
         .json({
           success: false,
-          message: "E-posta gönderme hatası: " + error.message,
+          message: error.message,
         });
     }
   } else {
