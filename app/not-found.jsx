57e1f8c5377d6479d4c2613e42f7378229379e03
Berke Güleryüz <berkeguleryuz@Berke-MacBook-Pro.local>
2024-04-05 15:54:48 +0300

    update

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,17 +1,29 @@
-"use client";
+import Container from "@/components/Container";
+import FadeIn from "@/components/FadeIn";
+import Link from "next/link";
+import React from "react";
 
-import Error from "next/error";
-
-// Render the default Next.js 404 page when a route
-// is requested that doesn't match the middleware and
-// therefore doesn't have a locale associated with it.
-
-export default function NotFound() {
+const NotFound = () => {
   return (
-    <html lang="en">
-      <body>
-        {/* <Error statusCode={404} /> */}
-      </body>
-    </html>
+    <Container className="flex h-full items-center pt-20 sm:pt-24 lg:pt-32">
+      <FadeIn className="flex flex-col items-center">
+        <p className="font-display text-4xl font-semibold text-neutral-950 sm:text-5xl">
+          404
+        </p>
+        <h1 className="mt-4 font-display text-2xl font-semibold text-neutral-950">
+          Page not found
+        </h1>
+        <p className="mt-2 text-sm text-neutral-600">
+          Sorry, we could ont find the page you are looking for.
+        </p>
+        <Link
+          href="/"
+          className="mt-4 text-sm font-semibold text-neutral-950 transition hover:text-neutral-700">
+          Go to the home page
+        </Link>
+      </FadeIn>
+    </Container>
   );
-}
+};
+
+export default NotFound;
