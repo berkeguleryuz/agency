9c6165c10d411918fc0be300b76673b5a45d45f3
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-06-28 12:05:51 +0200

    update

--- app/[locale]/work/page.jsx
+++ app/[locale]/work/page.jsx
@@ -1,20 +1,18 @@
 import PageIntro from "@/components/PageIntro";
 import { useTranslations } from "next-intl";
 import React from "react";
-import { unstable_setRequestLocale } from "next-intl/server";
+import { Marquee3D } from "@/components/3d";
 
 const WorkPage = () => {
-  unstable_setRequestLocale(locale);
+  // unstable_setRequestLocale(locale);
   const t = useTranslations("Work");
   return (
-    
     <div>
-      <PageIntro
-        eyebrow={t("Work0")}
-        title={t("Work1")}>
-        <p>
-        {t("Work2")}
-        </p>
+      <PageIntro eyebrow={t("Work0")} title={t("Work1")}>
+        <div className="mb-4">
+        <Marquee3D />
+        </div>
+        <p>{t("Work2")}</p>
       </PageIntro>
     </div>
   );
